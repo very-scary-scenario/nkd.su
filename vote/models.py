bc1996f2cd8116a25cec83e3acdf0f52389d7fed
Iain Dawson <iain@bldm.us>
2013-05-11 23:19:19 +0100

    Be spitefully permissive.

--- vote/models.py
+++ vote/models.py
@@ -808,8 +808,6 @@ class Track(models.Model):
         is not
         """
 
-        return "it's his birthday and he'll do what he wants to"
-
         try:
             return self.reason
         except AttributeError:
