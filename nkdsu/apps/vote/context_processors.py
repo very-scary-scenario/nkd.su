84901475f08e92f6f988b1646526c1d5e3f9a820
Iain Dawson <iain@bldm.us>
2013-12-30 17:06:56 +0000

    Bring back the nav bar.

--- nkdsu/apps/vote/context_processors.py
+++ nkdsu/apps/vote/context_processors.py
@@ -1,3 +1,5 @@
+from django.core.urlresolvers import reverse
+
 from .models import Show
 
 
@@ -6,7 +8,23 @@ def nkdsu_context_processor(request):
     Add common stuff to context.
     """
 
+    sections = [{
+        'name': section[0],
+        'url': section[1],
+    } for section in [
+        ('home', reverse('vote:index')),
+        ('archive', reverse('vote:archive')),
+        ('new tracks', reverse('vote:added')),
+        ('roulette', reverse('vote:roulette')),
+        ('stats', reverse('vote:stats')),
+        ('irc', 'irc://irc.rizon.net/NekoDesu'),
+        ('webchat',
+         'https://qchat.rizon.net/?channels=NekoDesu&uio=Mj10cnVlJjk9MAde'),
+        ('neko desu', 'http://thisisthecat.com/index.php/neko-desu'),
+    ]]
+
     return {
         'current_show': Show.current(),
         'path': request.path,
+        'sections': sections,
     }
