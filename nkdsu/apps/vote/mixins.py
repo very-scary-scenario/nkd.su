ffc298ac807ed56bc8803eef4c65377a9422dab8
Iain Dawson <iain@bldm.us>
2014-01-01 21:36:23 +0000

    Remove unnecessary call to tracks_sorted_by_votes()

--- nkdsu/apps/vote/mixins.py
+++ nkdsu/apps/vote/mixins.py
@@ -20,7 +20,6 @@ class CurrentShowMixin(object):
     def get_context_data(self):
         context = super(CurrentShowMixin, self).get_context_data()
         context['show'] = Show.current()
-        context['show'].tracks_sorted_by_votes()  # XXX
         return context
 
 
