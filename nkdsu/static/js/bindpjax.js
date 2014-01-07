eb122330b4b1f6d29e2e671a2b6deea3a829fb48
Iain Dawson <iain@bldm.us>
2014-01-07 23:14:52 +0000

    Make loading look less like a transition. Also way dumber.

--- nkdsu/static/js/bindpjax.js
+++ nkdsu/static/js/bindpjax.js
@@ -1,15 +1,28 @@
+var $loading;
+var messages = ['loading…', 'hang on…', 'please hold…', 'one moment…', 'be ready…', 'sit tight…', '*spinner*'];
+var loadingTimeout;
+
+function shuffleLoading() {
+  $loading.text(messages[Math.floor(Math.random() * messages.length)]);
+}
+
 $(document).ready(function(){
   $(document).pjax('a[href]', '#everything');
   $.pjax.defaults.timeout = 5000;
 
   var $body = $('body');
+  $loading = $('#loading');
 
   $(document).on('pjax:send', function() {
-    $body.addClass('loading');
+    shuffleLoading();
+    loadingTimeout = setTimeout(function() {
+      $body.addClass('loading');
+    }, 500);
   });
 
   $(document).on('pjax:success', function() {
     $body.removeClass('loading');
+    clearTimeout(loadingTimeout);
   });
 
   $(document).on('pjax:end', function() {
