bd59bc506286a6ff2ef657526d46d1cf2c686053
Iain Dawson <iain@bldm.us>
2014-03-02 23:01:25 +0000

    Don't freak out about the occasional non-RGB image we might come across.

--- nkdsu/apps/vote/models.py
+++ nkdsu/apps/vote/models.py
@@ -714,12 +714,13 @@ class Track(CleanOnSaveMixin, models.Model):
             return
 
         input_image = Image.open(StringIO(requests.get(image_url).content))
+        rgb_image = input_image.convert('RGB')
 
         # in almost all circumstances, it will be width that determines display
         # size, so we should set our blur radius relative to that.
-        radius = input_image.size[0] / 130
+        radius = rgb_image.size[0] / 130
 
-        blurred = input_image.filter(ImageFilter.GaussianBlur(radius=radius))
+        blurred = rgb_image.filter(ImageFilter.GaussianBlur(radius=radius))
 
         suffix = '.jpg'
         temp_file = NamedTemporaryFile(delete=True, suffix=suffix)
