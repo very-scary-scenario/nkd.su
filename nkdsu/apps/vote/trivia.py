f6ca50edf79afac22c7bc83c669add9b387d5859
Iain Dawson <iain@colons.co>
2014-07-21 15:16:42 +0100

    Remove some more old references.
    cc. #115

--- nkdsu/apps/vote/trivia.py
+++ nkdsu/apps/vote/trivia.py
@@ -1,21 +1,12 @@
 questions = {
-    'We have always treaded ____':
-    r'.*borderlands?.*',
-
     'JIBUN ____':
     r'WO+',
 
     'Feel so ____':
     r'moo+n',
 
-    'Do you ____ online?':
-    r'(shop)|(wub)',
-
     '____ Daikazoku':
     r'dango',
-
-    'BA BA BA BADIADIA BA BA ____':
-    r'[BADIYUH !]+',
 
     'Does Peter have good taste in anime?':
     r'((ha)+)|(yes)|(no)',
