3067c805ea8d1157eb7d55cf62a8c91b116a8dfc
h0m54r <h0m54r@twitter.com>
2019-12-29 23:28:49 +0000

    pull out "all artists" and "all anime titles" into class methods on Track to allow reuse

--- nkdsu/apps/vote/models.py
+++ nkdsu/apps/vote/models.py
@@ -529,6 +529,20 @@ class Track(CleanOnSaveMixin, models.Model):
             raise ValidationError('{track} is not hidden but has no revealed '
                                   'date'.format(track=self))
 
+    @classmethod
+    def all_anime_titles(cls):
+        return set(
+            (t.role_detail.get('anime', '') for t in cls.objects.all())
+        )
+
+    @classmethod
+    def all_artists(cls):
+        return set(
+            a['name']
+            for t in cls.objects.all()
+            for a in t.artists()
+        )
+
     @memoize
     def is_new(self):
         return Show.current() == self.show_revealed()
