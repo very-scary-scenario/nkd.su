ebac16f489631b2cfc71119086c58f956c699247
h0m54r <h0m54r@twitter.com>
2019-12-30 01:54:34 +0000

    flag up metadata additions and changes that seem to introduce data inconsistency

--- nkdsu/apps/vote/update_library.py
+++ nkdsu/apps/vote/update_library.py
@@ -1,14 +1,72 @@
 #!/usr/bin/env python
 
 from models import Track
+from Levenshtein import ratio
 from django.utils.timezone import get_default_timezone, make_aware
+
+
+def check_closeness_against_list(name, canonical_names):
+    best_closeness, best_match = 0.7, None
+    if name and name not in canonical_names:
+        for canonical_name in canonical_names:
+            closeness = ratio(unicode(name.lower()),
+                              unicode(canonical_name.lower()))
+            if closeness > best_closeness:
+                best_closeness = closeness
+                best_match = canonical_name
+
+    return best_match
+
+
+def metadata_consistency_checks(db_track, all_anime_titles, all_artists):
+    warnings = []
+    track_anime = db_track.role_detail.get('anime', '')
+    track_role = db_track.role_detail.get('role', '')
+
+    if not track_anime and not db_track.inudesu:
+        warnings.append({
+            'field': 'anime',
+            'message': 'field is missing'
+        })
+
+    if not track_role and not db_track.inudesu:
+        warnings.append({
+            'field': 'role',
+            'message': 'field is missing'
+        })
+
+    if track_anime:
+        match = check_closeness_against_list(track_anime, all_anime_titles)
+        if match:
+            warnings.append({
+                'field': 'anime',
+                'message': (
+                    u'"{track_anime}" was not found in the database, but it '
+                    u'looks similar to "{canonical_anime}", which is'
+                ).format(track_anime=track_anime, canonical_anime=match)
+            })
+
+    for artist in db_track.artists():
+        artist_name = artist.get('name', '')
+        match = check_closeness_against_list(artist_name, all_artists)
+        if match:
+            warnings.append({
+                'field': 'artist',
+                'message': (
+                    u'"{track_artist}" was not found in the database, but it '
+                    u'looks similar to "{canonical_artist}", which is'
+                ).format(track_artist=artist, canonical_artist=match)
+            })
+
+    return warnings
 
 
 def update_library(tree, dry_run=False, inudesu=False):
     changes = []
     alltracks = Track.objects.filter(inudesu=inudesu)
+    all_anime_titles = Track.all_anime_titles()
+    all_artists = Track.all_artists()
     tracks_kept = []
-
     for tid in tree['Tracks']:
         changed = False
         new = False
@@ -74,6 +132,11 @@ def update_library(tree, dry_run=False, inudesu=False):
             db_track.composer = t.get('Composer', '')
             db_track.added = added
             db_track.inudesu = inudesu
+            warnings.extend(
+                metadata_consistency_checks(
+                    db_track, all_anime_titles, all_artists
+                )
+            )
 
         if new:
             if not inudesu:
@@ -98,7 +161,7 @@ def update_library(tree, dry_run=False, inudesu=False):
             db_track.save()
 
         tracks_kept.append(db_track)
-
+    import pdb; pdb.set_trace()
     for track in [tr for tr in alltracks
                   if tr not in tracks_kept and not tr.hidden]:
         changes.append({
