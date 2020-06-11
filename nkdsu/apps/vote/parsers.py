0d5e84354c51713d3df0e96cb79d2a282a5381c7
colons <colons@colons.co>
2020-06-11 18:33:14 +0100

    Accept oxford commas.

--- nkdsu/apps/vote/parsers.py
+++ nkdsu/apps/vote/parsers.py
@@ -17,7 +17,7 @@ class ArtistLexer(Lexer):
     LPAREN = r'\s\('
     RPAREN = r'\)(?=\s|,|\)|$)'
     CV = r'(CV[.:]|[Vv]ocal:|[Mm]ain\svocals?:|[Cc]omposed\sby|[Ff]rom)\s+'
-    COMMA = r',\s+'
+    COMMA = r',(\sand)?\s+'
     SPACE = r'\s+'
     ARTIST_COMPONENT = (
         r'('
