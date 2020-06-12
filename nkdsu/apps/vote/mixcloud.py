de1a04e2653f8c3a51dd61c8ead8a7fcab6bf48c
colons <colons@colons.co>
2020-06-12 10:13:25 +0100

    Try to be better about getting old Mixcloud links.

--- nkdsu/apps/vote/mixcloud.py
+++ nkdsu/apps/vote/mixcloud.py
@@ -4,20 +4,40 @@ from django.conf import settings
 from django.core.cache import cache
 
 
-def _api_resp():
-    ck = 'mixcloud:feed'
+TIMEOUT = 4
+
+
+def _get_feed_items():
+    page = requests.get(
+        settings.MIXCLOUD_FEED_URL,
+        params={'since': '0', 'limit': '100'},
+        timeout=TIMEOUT,
+    ).json()
+
+    # we should page through for a while, but at a certain point it probably
+    # isn't worth the load times
+    for attempt in range(20):
+        yield from page['data']
+
+        next_page_url = page['paging'].get('next')
+        if next_page_url is None:
+            break
+
+        page = requests.get(next_page_url, timeout=TIMEOUT).json()
+
+
+def _get_items():
+    # cache wrapper for _get_feed_items()
+
+    ck = 'mixcloud:all-items'
     hit = cache.get(ck)
+
     if hit:
         return hit
 
     try:
-        rv = requests.get(
-            settings.MIXCLOUD_FEED_URL,
-            # we should probably be paginating here to ensure we get everything
-            params={'since': '0', 'limit': '100'},
-            timeout=5,
-        ).json()
-        cache.set(ck, rv, 60*5)
+        rv = list(_get_feed_items())
+        cache.set(ck, rv, 60*20)
         return rv
     except requests.RequestException:
         # problem with the Mixcloud API; panic
@@ -25,16 +45,16 @@ def _api_resp():
 
 
 def cloudcasts_for(date):
-    resp = _api_resp()
+    items = _get_items()
 
-    if resp is None:
+    if items is None:
         return None
 
     hunting_for = date.strftime('%d/%m/%Y')
     cloudcasts = []
 
-    for datum in resp['data']:
-        for cloudcast in datum.get('cloudcasts', []):
+    for item in items:
+        for cloudcast in item.get('cloudcasts', []):
             if hunting_for in cloudcast['name']:
                 cloudcasts.append(cloudcast)
 
