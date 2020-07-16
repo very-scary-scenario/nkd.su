6ecbb153fff1b7d79f6d67385d0db9ccb44e5b4f
colons <colons@colons.co>
2020-07-16 23:59:59 +0100

    Don't override dark mode form handling if Fetch isn't available.

--- nkdsu/static/js/dark.js
+++ nkdsu/static/js/dark.js
@@ -1,20 +1,22 @@
 function bindDarkModeForm() {
   var darkModeForm = document.getElementById('dark-mode-settings');
-  darkModeForm.addEventListener('submit', function(e) {
-    e.preventDefault();
-    var data = new FormData(darkModeForm);
-    if (e.submitter) {
-      data.set('mode', e.submitter.getAttribute('value'));
-    }
+  if (fetch !== undefined) {
+    darkModeForm.addEventListener('submit', function(e) {
+      e.preventDefault();
+      var data = new FormData(darkModeForm);
+      if (e.submitter) {
+        data.set('mode', e.submitter.getAttribute('value'));
+      }
 
-    fetch(darkModeForm.getAttribute('action'),
-      {method: 'post', body: data, redirect: 'manual'})
-      .then(function(response) {
-        console.log(response);
-      });
+      fetch(darkModeForm.getAttribute('action'),
+        {method: 'post', body: data, redirect: 'manual'})
+        .then(function(response) {
+          console.log(response);
+        });
 
-    document.body.setAttribute('data-dark-mode', data.get('mode'));
-  });
+      document.body.setAttribute('data-dark-mode', data.get('mode'));
+    });
+  }
 }
 
 $(document).ready(bindDarkModeForm);
