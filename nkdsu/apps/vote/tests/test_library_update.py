0f651e9c1557f560ae41266303a8e55bcaf92811
colons <colons@colons.co>
2021-05-22 10:59:28 +0100

    Fix the library update tests.

--- nkdsu/apps/vote/tests/test_library_update.py
+++ nkdsu/apps/vote/tests/test_library_update.py
@@ -15,6 +15,7 @@ SINGLE_TRACK_XML = '''
         <key>Artist</key><string>{artist}</string>
         {album_xml}
         <key>Kind</key><string>MPEG audio file</string>
+        <key>Composer</key><string>{composer}</string>
         <key>Total Time</key><integer>{time}</integer>
         <key>Date Added</key><date>{added}</date>
         <key>Persistent ID</key><string>{hex_id}</string>
@@ -39,7 +40,7 @@ PLIST_TEMPLATE = '''<?xml version="1.0" encoding="UTF-8"?>
 '''
 
 TrackMeta = namedtuple(
-    'TrackMeta', ('key', 'name', 'artist', 'album', 'time', 'added', 'hex_id')
+    'TrackMeta', ('key', 'name', 'artist', 'album', 'composer', 'time', 'added', 'hex_id')
 )
 
 DEFAULT_TRACKS = [
@@ -48,6 +49,7 @@ DEFAULT_TRACKS = [
         "Canpeki Shinakya! (RO-KYU-BU! Character Song: Saki)",
         "Hikasa Youko",
         "RO-KYU-BU! Character Songs 03 - Nagatsuka Saki|Lucky Star ED4",
+        "idk, someone",
         "225044",
         "2012-12-15T18:51:21Z",
         "0007C3F2760E0541"
@@ -57,6 +59,7 @@ DEFAULT_TRACKS = [
         "Hajimete no iro (The World God Only Knows EP4 Insert)",
         "Sakurai Tomo",
         "Kami Nomi zo Shury Sekai Character CD. - Asuka Sora",
+        "someone else",
         "216894",
         "2010-12-03T17:24:49Z",
         "001D8E8B3A5DDDB9"
@@ -66,6 +69,7 @@ DEFAULT_TRACKS = [
         "Runrunriru Ranranrara (Maria Holic Alive OP)",
         "Kobayashi Yuu",
         "Maria Holic Alive OP and ED Album - Runrunriru Ranranrara",
+        "person",
         "238393",
         "2011-05-25T13:24:48Z",
         "0028E1FE6D1141B7"
@@ -75,6 +79,7 @@ DEFAULT_TRACKS = [
         "Neguse (Tamako Market ED)",
         "Suzaki Aya",
         "Tamako Market ED Single - Neguse",
+        "folks",
         "232907",
         "2013-01-26T12:39:37Z",
         "00340A1B035648A9"
@@ -84,6 +89,7 @@ DEFAULT_TRACKS = [
         'Hands Around My Throat (Animatrix "Beyond" Insert)',
         "Death in Vegas",
         "The Animatrix - The Album (OST)",
+        "folks",
         "305893",
         "2011-03-19T19:12:21Z",
         "00555AF6AC71CB70"
@@ -93,6 +99,7 @@ DEFAULT_TRACKS = [
         "Day Game (Angel Beats Character Song: Girls Dead Monster)",
         "Girls Dead Monster (LiSA Singing)",
         "Ichiban no Takaramono ~Yui final ver.~",
+        "",
         "293146",
         "2010-12-05T12:55:06Z",
         "00590FF313BD5557"
@@ -102,6 +109,7 @@ DEFAULT_TRACKS = [
         "ft. (Fairy Tail OP3)",
         "FUNKIST",
         "Fairy Tail OP ED Theme Songs Vol.1",
+        "",
         "211905",
         "2012-11-16T00:05:05Z",
         "00D12BB6D5ED72A7"
@@ -129,8 +137,9 @@ class LibraryUpdateTest(TestCase):
             tracks=[self.get_track_xml(track) for track in tracks]
         ).encode()
 
-    def library_plus_one_track(self, name, artist, time, added, hex_id,
-                               album=None):
+    def library_plus_one_track(
+        self, name, artist, composer, time, added, hex_id, album=None,
+    ):
         return plistlib.loads(
             self.get_library_xml(
                 DEFAULT_TRACKS + [TrackMeta(
@@ -138,9 +147,10 @@ class LibraryUpdateTest(TestCase):
                     name,
                     artist,
                     album,
+                    composer,
                     time,
                     added,
-                    hex_id
+                    hex_id,
                 )]
             )
         )
@@ -150,7 +160,9 @@ class LibraryUpdateTest(TestCase):
         for track_id, track in enumerate(tracks):
             if track.hex_id == hex_id:
                 track_dict = track._asdict()
-                for field in ('name', 'artist', 'album', 'time', 'added'):
+                for field in (
+                    'name', 'artist', 'album', 'composer', 'time', 'added'
+                ):
                     if field in new_meta:
                         track_dict[field] = new_meta[field]
                 tracks[track_id] = TrackMeta(**track_dict)
@@ -162,6 +174,7 @@ class LibraryUpdateDryRunTest(LibraryUpdateTest):
         tree = self.library_plus_one_track(
             "Koi Hana (Kaichou wa Maid-sama! Character Song)",
             "Hanazawa Kana and Kobayashi Yuu",
+            "more people",
             "254000",
             "2010-09-02T02:00:00Z",
             "ADDD70C2D748ECC7"
@@ -181,6 +194,7 @@ class LibraryUpdateDryRunTest(LibraryUpdateTest):
         tree = self.library_plus_one_track(
             "Universal Bunny (Macross Frontier Movies Insert Song)",
             "Sheryl Nome starring May'n",
+            "a giant robot",
             "357000",
             "2010-11-11T02:00:00Z",
             "0D096C693DA38F51"
@@ -200,6 +214,7 @@ class LibraryUpdateDryRunTest(LibraryUpdateTest):
         tree = self.library_plus_one_track(
             "23:50 (Angel Beats Character Song - Girls Dead Monster)",
             "Girls Dead Monster (Lisa Singing)",
+            "a moltres gijinka",
             "261000",
             "2010-06-28T02:00:00Z",
             "765682E348A46C30"
@@ -221,6 +236,7 @@ class LibraryUpdateDryRunTest(LibraryUpdateTest):
         tree = self.library_plus_one_track(
             "Koi Hana (Kaichou wa Maid-sama! Character Song)",
             "Kana Hanazawa and Yuu Kobayashi",
+            "a dragon",
             "254000",
             "2010-09-02T02:00:00Z",
             "ADDD70C2D748ECC7"
@@ -241,6 +257,7 @@ class LibraryUpdateDryRunTest(LibraryUpdateTest):
         tree = self.library_plus_one_track(
             "Koi Hana (Kaichou wa Maid-sama! Character Song)",
             "Kana Hanazawa and Yui Kobayashi",
+            "imagine dragons",
             "254000",
             "2010-09-02T02:00:00Z",
             "ADDD70C2D748ECC7"
@@ -261,6 +278,7 @@ class LibraryUpdateDryRunTest(LibraryUpdateTest):
         tree = self.library_plus_one_track(
             "All 4 You (The World God Only Knows Insert Song EP5)",
             "Kanon Nakagawa (Touyama Nao)",
+            "the friendly blobs from locoroco",
             "250000",
             "2011-03-07T02:00:00Z",
             "F2FDCB0308431FB4"
@@ -280,6 +298,7 @@ class LibraryUpdateDryRunTest(LibraryUpdateTest):
         tree = self.library_plus_one_track(
             "All 4 You (The world G-d Only Knows Insert Song EP5)",
             "Kanon Nakagawa (Touyama Nao)",
+            "the protagonist of zoe mode's 'crush'",
             "250000",
             "2011-03-07T02:00:00Z",
             "F2FDCB0308431FB4"
@@ -301,6 +320,7 @@ class LibraryUpdateDryRunTest(LibraryUpdateTest):
         tree = self.library_plus_one_track(
             "Dramatic Market Ride (Tamako makret OP)",
             "Suzako Aya",
+            "sergei",
             "260000",
             "2013-01-26T02:00:00Z",
             "4ACC4F068C0E4C16"
@@ -341,6 +361,7 @@ class LibraryUpdateDryRunTest(LibraryUpdateTest):
         tree = self.library_plus_one_track(
             "Born This Way (feat. YZERR, Vingo &amp; Bark) (Kengan Ashura ED)",
             "BAD HOP feat.YZERR,Vingo＆Bark",
+            "popplio",
             "168000",
             "2011-03-07T02:00:00Z",
             "2A25990D66A020E6",
@@ -366,6 +387,7 @@ class LibraryUpdateWetRunTest(LibraryUpdateTest):
         tree = self.library_plus_one_track(
             "Koi Hana (Kaichou wa Maid-sama! Character Song)",
             "Hanazawa Kana and Kobayashi Yuu",
+            "I.R.I.S.",
             "254000",
             "2010-09-02T02:00:00Z",
             "ADDD70C2D748ECC7"
