f36eb2857b70175ac5fe3defc9d44df8c357aa5e
colons <colons@colons.co>
2021-10-21 19:13:26 +0100

    Wrap track expansion in a deferred function.

--- nkdsu/static/js/expand.js
+++ nkdsu/static/js/expand.js
@@ -1,14 +1,18 @@
-const trackElements = document.querySelectorAll('.track')
+function bindAllTrackExpansion() {
+  const trackElements = document.querySelectorAll('.track')
 
-function bindTrackExpansion(trackElement) {
-  const toggleElement = document.createElement('a')
-  toggleElement.classList.add('expansion-toggle')
-  toggleElement.addEventListener('click', () => {
-    trackElement.classList.toggle('expanded')
-  })
-  trackElement.appendChild(toggleElement)
+  function bindTrackExpansion(trackElement) {
+    const toggleElement = document.createElement('a')
+    toggleElement.classList.add('expansion-toggle')
+    toggleElement.addEventListener('click', () => {
+      trackElement.classList.toggle('expanded')
+    })
+    trackElement.appendChild(toggleElement)
+  }
+
+  for (let i = 0; i < trackElements.length; i++) {
+    bindTrackExpansion(trackElements[i])
+  }
 }
 
-for (let i = 0; i < trackElements.length; i++) {
-  bindTrackExpansion(trackElements[i])
-}
+document.addEventListener('DOMContentLoaded', bindAllTrackExpansion)
