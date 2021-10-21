84b92c31c7e8dbe847266109eb206a73f15e5cf3
colons <colons@colons.co>
2021-10-21 19:12:09 +0100

    Placate eslint.

--- nkdsu/static/js/expand.js
+++ nkdsu/static/js/expand.js
@@ -1,14 +1,14 @@
-let trackElements = document.querySelectorAll('.track')
+const trackElements = document.querySelectorAll('.track')
 
 function bindTrackExpansion(trackElement) {
-  let toggleElement = document.createElement('a')
+  const toggleElement = document.createElement('a')
   toggleElement.classList.add('expansion-toggle')
-  toggleElement.addEventListener('click', function () {
+  toggleElement.addEventListener('click', () => {
     trackElement.classList.toggle('expanded')
   })
   trackElement.appendChild(toggleElement)
 }
 
-for (var i = 0; i < trackElements.length; i++) {
+for (let i = 0; i < trackElements.length; i++) {
   bindTrackExpansion(trackElements[i])
 }
