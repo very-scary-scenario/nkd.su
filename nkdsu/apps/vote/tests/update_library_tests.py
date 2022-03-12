d4c729f2db2e04c273e2b29664237c09b0fc8062
colons <colons@colons.co>
2022-03-12 22:14:34 +0000

    Complete this check that inudesu influences the role/anime requirement.

--- nkdsu/apps/vote/tests/update_library_tests.py
+++ nkdsu/apps/vote/tests/update_library_tests.py
@@ -7,9 +7,19 @@ from ..update_library import metadata_consistency_checks
 class MetadataConsistencyCheckTest(TestCase):
     def test_anime_and_role_required_if_inudesu_false(self):
         self.assertEqual(
+            metadata_consistency_checks(Track(id3_title='Complete role (Some kind of anime)'), [], []),
+            [],
+        )
+
+        self.assertEqual(
             metadata_consistency_checks(Track(id3_title='No role at all'), [], []),
             [
                 {'field': 'anime', 'message': 'field is missing'},
                 {'field': 'role', 'message': 'field is missing'},
             ],
+        )
+
+        self.assertEqual(
+            metadata_consistency_checks(Track(id3_title='no role, but who cares', inudesu=True), [], []),
+            [],
         )
