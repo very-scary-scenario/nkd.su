69bd635917b74ef24426d30175c8ad8659d841ef
colons <colons@colons.co>
2022-03-12 22:25:03 +0000

    Test artist consistency checks.
    This isn't really as unit-test-ish as it should be; the logic of
    metadata_consistency_checks() is concrete, but
    check_closeness_against_list() is fuzzy. This checks the intent, though,
    and that's what matters I think.

--- nkdsu/apps/vote/tests/update_library_tests.py
+++ nkdsu/apps/vote/tests/update_library_tests.py
@@ -23,3 +23,57 @@ class MetadataConsistencyCheckTest(TestCase):
             metadata_consistency_checks(Track(id3_title='no role, but who cares', inudesu=True), [], []),
             [],
         )
+
+    def test_slight_artist_mismatch(self):
+        self.assertEqual(
+            metadata_consistency_checks(Track(
+                id3_title='title (role)',
+                id3_artist='an artist of some kind',
+            ), [], [
+                'an artist of some kind',
+            ]),
+            [],
+        )
+
+        self.assertEqual(
+            metadata_consistency_checks(Track(
+                id3_title='title (role)',
+                id3_artist='an artist of some knid',
+            ), [], [
+                'an artist of some kind',
+            ]),
+            [{
+                'field': 'artist',
+                'message': (
+                    '"an artist of some knid" was not found in the database, but it looks similar to '
+                    '"an artist of some kind"'
+                ),
+            }],
+        )
+
+        self.assertEqual(
+            metadata_consistency_checks(Track(
+                id3_title='title (role)',
+                id3_artist='a completely different artist',
+            ), [], [
+                'an artist of some kind',
+            ]),
+            [],
+        )
+
+        # the artist name but with the words reversed is a special case that we also handle:
+        self.assertEqual(
+            metadata_consistency_checks(Track(
+                id3_title='title (role)',
+                id3_artist='kind some of artist an',
+            ), [], [
+                'an artist of some kind',
+            ]),
+            [{
+                'field': 'artist',
+                'message': (
+                    '"kind some of artist an" was not found in the database, but it looks similar to '
+                    '"an artist of some kind"'
+                ),
+            }],
+        )
