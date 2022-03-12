311a301b614c0e86acf5a6f5532dbc1a207a7d12
colons <colons@colons.co>
2022-03-12 23:12:20 +0000

    Combine the library update tests that we have.

--- nkdsu/apps/vote/tests/test_library_update.py
+++ nkdsu/apps/vote/tests/test_library_update.py
@@ -5,7 +5,7 @@ from typing import Any, Dict, Iterable, Optional
 from django.test import TestCase
 
 from ..models import Track
-from ..update_library import update_library
+from ..update_library import metadata_consistency_checks, update_library
 
 
 SINGLE_TRACK_XML = '''
@@ -444,3 +444,78 @@ class LibraryUpdateWetRunTest(LibraryUpdateTest):
         update_library(tree, dry_run=False)
         db_track = Track.objects.get(id=hex_id)
         self.assertEqual(db_track.artist, 'Death in Vegas')
+
+
+class MetadataConsistencyCheckTest(TestCase):
+    def test_anime_and_role_required_if_inudesu_false(self) -> None:
+        self.assertEqual(
+            metadata_consistency_checks(Track(id3_title='Complete role (Some kind of anime)'), [], [], []),
+            [],
+        )
+
+        self.assertEqual(
+            metadata_consistency_checks(Track(id3_title='No role at all'), [], [], []),
+            [
+                {'field': 'anime', 'message': 'field is missing'},
+                {'field': 'role', 'message': 'field is missing'},
+            ],
+        )
+
+        self.assertEqual(
+            metadata_consistency_checks(Track(id3_title='no role, but who cares', inudesu=True), [], [], []),
+            [],
+        )
+
+    def test_slight_artist_mismatch(self) -> None:
+        self.assertEqual(
+            metadata_consistency_checks(Track(
+                id3_title='title (role)',
+                id3_artist='an artist of some kind',
+            ), [], [
+                'an artist of some kind',
+            ], []),
+            [],
+        )
+
+        self.assertEqual(
+            metadata_consistency_checks(Track(
+                id3_title='title (role)',
+                id3_artist='an artist of some knid',
+            ), [], [
+                'an artist of some kind',
+            ], []),
+            [{
+                'field': 'artist',
+                'message': (
+                    '"an artist of some knid" was not found in the database, but it looks similar to '
+                    '"an artist of some kind"'
+                ),
+            }],
+        )
+
+        self.assertEqual(
+            metadata_consistency_checks(Track(
+                id3_title='title (role)',
+                id3_artist='a completely different artist',
+            ), [], [
+                'an artist of some kind',
+            ], []),
+            [],
+        )
+
+        # the artist name but with the words reversed is a special case that we also handle:
+        self.assertEqual(
+            metadata_consistency_checks(Track(
+                id3_title='title (role)',
+                id3_artist='kind some of artist an',
+            ), [], [
+                'an artist of some kind',
+            ], []),
+            [{
+                'field': 'artist',
+                'message': (
+                    '"kind some of artist an" was not found in the database, but it looks similar to '
+                    '"an artist of some kind"'
+                ),
+            }],
+        )
