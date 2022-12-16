f4279671b58b4109d5551c70aef58cfd3224bd1f
colons <colons@colons.co>
2022-12-16 16:56:12 +0000

    Only show TWITTER.md contents when someone's trying to make a request.
    Next step: show a 'legacy' Twitter intent URL.

--- nkdsu/views.py
+++ nkdsu/views.py
@@ -1,5 +1,10 @@
-from django.contrib.auth import views as auth_views
+from typing import Any
+from urllib.parse import parse_qs, urlparse
 
+from django.contrib.auth import views as auth_views
+from django.shortcuts import get_object_or_404
+
+from .apps.vote.models import Track
 from .mixins import MarkdownView
 
 
@@ -7,3 +12,28 @@ class LoginView(MarkdownView, auth_views.LoginView):
     template_name = auth_views.LoginView.template_name
     title = 'login'
     filename = 'TWITTER.md'
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+
+        twitter_vote_url = None
+        tracks = []
+
+        query_string = urlparse(context[self.redirect_field_name]).query
+        query = parse_qs(query_string)
+
+        if query is not None:
+            tracks = [
+                get_object_or_404(Track, pk=pk)
+                for pk_string in query.get('t', [])
+                for pk in pk_string.split(',')
+            ]
+
+        if tracks:
+            print(NotImplementedError(tracks))  # XXX create a twitter vote URL
+
+        return {
+            **context,
+            'is_vote': bool(tracks),  # this is naive, but we don't use ?t= for a lot
+            'twitter_vote_url': twitter_vote_url,
+        }
