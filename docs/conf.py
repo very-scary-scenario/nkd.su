a58893c3da9055874c54beb368a67787b5395fb9
colons <colons@colons.co>
2023-02-11 15:08:51 +0000

    Use the Read the Docs theme.

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,9 @@ author = 'Very Scary Scenario'
 # General
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions: list[str] = []
+extensions = [
+    'sphinx_rtd_theme',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -13,5 +15,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # HTML output
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
