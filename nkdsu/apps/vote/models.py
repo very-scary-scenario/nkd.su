f761167da736d6404a5724bba72793a588f644a4
colons <colons@colons.co>
2023-10-07 17:18:15 +0100

    Placate black.

--- nkdsu/apps/vote/models.py
+++ nkdsu/apps/vote/models.py
@@ -682,7 +682,12 @@ class Track(CleanOnSaveMixin, models.Model):
     def clean(self) -> None:
         # these checks can be deleted once we're on django 4.2, since they're enforced in a constraint
         # (be sure to preserve the nice error messages, though)
-        if (not self.inudesu) and (not self.hidden) and (not self.archived) and (not self.revealed):
+        if (
+            (not self.inudesu)
+            and (not self.hidden)
+            and (not self.archived)
+            and (not self.revealed)
+        ):
             raise ValidationError(
                 '{track} is visible but has no revealed date'.format(track=self)
             )
