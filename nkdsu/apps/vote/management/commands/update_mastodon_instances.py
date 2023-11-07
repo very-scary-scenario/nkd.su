a9701ce0153e1c648d1c01787068d9b237d8956a
colons <colons@colons.co>
2023-11-07 19:02:53 +0000

    Break long domains up to placate flake8.
    This is silly. I should just be writing these to a file.

--- nkdsu/apps/vote/management/commands/update_mastodon_instances.py
+++ nkdsu/apps/vote/management/commands/update_mastodon_instances.py
@@ -13,6 +13,23 @@ SOURCE_INSTANCES = {
 }
 
 
+def fmt(domain: str) -> str:
+    """
+    Break a domain into chunks, so that black is able to combine or rejoin it
+    as necessary, but only if it's particularly long.
+    """
+
+    if len(domain) > 100:
+        return '(\n        {}\n    )'.format(
+            '\n        '.join(
+                repr(('' if i == 0 else '.') + chunk)
+                for i, chunk in enumerate(domain.split('.'))
+            )
+        )
+    else:
+        return repr(domain)
+
+
 class Command(BaseCommand):
     def write_instances(self, instances: list[str]) -> None:
         module_path = os.path.join(
@@ -24,9 +41,11 @@ class Command(BaseCommand):
                 "# this file is populated by running `python manage.py"
                 " update_mastodon_instances`\n# do not edit it by hand\n\n"
             )
+            splitter = ',\n    '
             f.write(
-                "MASTODON_INSTANCES: set[str] = {"
-                f" {', '.join((repr(i) for i in instances))} }}"
+                "MASTODON_INSTANCES: set[str] = {\n"
+                f"    {splitter.join((fmt(i) for i in instances))},\n"
+                "}"
             )
 
         subprocess.check_call(['black', module_path])
