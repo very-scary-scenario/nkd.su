7833ce5dec54236e61656bec94f1bb1406e70704
colons <colons@colons.co>
2024-02-11 14:20:59 +0000

    Include more information iin our anime model.

--- nkdsu/apps/vote/anime.py
+++ nkdsu/apps/vote/anime.py
@@ -1,20 +1,30 @@
-from pydantic import BaseModel
+from typing import Literal, Optional
 import os
 
+from pydantic import BaseModel
+from typing_extensions import TypedDict
 import ujson
+
+
+class Season(TypedDict):
+    year: Optional[int]
+    season: Literal['SPRING', 'SUMMER', 'FALL', 'WINTER', 'UNDEFINED']
 
 
 class Anime(BaseModel):
     title: str
     synonyms: list[str]
+    sources: list[str]
+    animeSeason: Season
 
 
-with open(os.path.join(
-    os.path.dirname(__file__), 'data', 'mpaod', 'anime-offline-database.json',
-), 'rt') as aodf:
-    by_title = {
-        a['title']: Anime(
-            title=a['title'],
-            synonyms=a['synonyms'],
-        ) for a in ujson.load(aodf)['data']
-    }
+with open(
+    os.path.join(
+        os.path.dirname(__file__),
+        'data',
+        'mpaod',
+        'anime-offline-database.json',
+    ),
+    'rt',
+) as aodf:
+    by_title = {a['title']: Anime(**a) for a in ujson.load(aodf)['data']}
