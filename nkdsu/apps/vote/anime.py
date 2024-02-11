576a98d00ee8a78f22863fd62217298b1202ab9f
colons <colons@colons.co>
2024-02-11 17:18:02 +0000

    Create a mapping from anime alt titles to our canonical titles.

--- nkdsu/apps/vote/anime.py
+++ nkdsu/apps/vote/anime.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 from typing_extensions import TypedDict
 import ujson
 
-from .utils import camel_to_snake
+from .utils import cached, camel_to_snake
 
 
 class Season(TypedDict):
@@ -54,3 +54,45 @@ def anime(title: str) -> Optional[Anime]:
     """
 
     return by_title.get(title) or by_synonym.get(title)
+
+
+@cached(60 * 60, 'fuzzy-aliases')
+def fuzzy_nkdsu_aliases() -> dict[str, str]:
+    """
+    Return a dict of ``{alias: nkdsu_title}`` where ``nkdsu_title`` is an anime
+    included in the nkd.su database, and ``alias`` is a lowercased alternative
+    title for the ``nkdsu_title`` it points to.
+
+    >>> from pprint import pprint
+    >>> from nkdsu.apps.vote.models import Track
+    >>> from django.utils.timezone import now
+    >>> defaults = dict(
+    ...     id3_artist='someone', hidden=False, inudesu=False, added=now(), revealed=now()
+    ... )
+
+    With some anime titles that don't have very many synonyms:
+
+    >>> Track.objects.create(**defaults, id='1', id3_title='song (Eiji OP1)')
+    <Track: ‘song’ (Eiji OP1) - someone>
+    >>> Track.objects.create(**defaults, id='2', id3_title='ditty (◯ ED1)')
+    <Track: ‘ditty’ (◯ ED1) - someone>
+    >>> pprint(fuzzy_nkdsu_aliases())
+    {'"eiji"': 'Eiji',
+     'circle': '◯',
+     'eiji': 'Eiji',
+     'o (sawako kabuki)': '◯',
+     '°': '◯',
+     '○': '◯',
+     '◯': '◯',
+     '「エイジ」': 'Eiji',
+     'エイジ': 'Eiji'}
+    """
+
+    from .models import Track
+
+    return {
+        alt_title.lower(): title
+        for anime, title in map(lambda t: (anime(t), t), Track.all_anime_titles())
+        if anime is not None
+        for alt_title in anime.titles()
+    }
