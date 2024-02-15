adb19f46431533fbfb207514a1d39ac087e15e77
colons <colons@colons.co>
2024-02-15 21:51:06 +0000

    Don't try to delete SOURCE_DATE_EPOCH if it's not present.

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,8 @@ django.setup()
 project = 'nkd.su'
 
 # sphinx automatically replaces four-digit clusters in copyright strings if this is set:
-del os.environ['SOURCE_DATE_EPOCH']
+if 'SOURCE_DATE_EPOCH' in os.environ:
+    del os.environ['SOURCE_DATE_EPOCH']
 
 project_copyright = f'{_commit_year_range()}, colons and nkd.su contributors'
 author = 'Very Scary Scenario'
